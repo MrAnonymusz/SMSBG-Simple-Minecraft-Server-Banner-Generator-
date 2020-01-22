582d941096d069558b02a89969817bca46087bbe
MrAnonymusz <r1pp3r444@gmail.com>
2020-01-22 19:39:29 +0200

    Updated to Version 1.1

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+
+$config = [
+  'app_key' => 'K6TdeFXHuqZC7Fam',
+  'enable_key' => false,
+  'query_api' => 'https://api.mcsrvstat.us/2/',
+  'app_url' => 'http://localhost/banner/',
+  'fonts' => ['ubuntu', 'roboto', 'minecraftia'],
+  'defaults' => [
+    'font' => 'ubuntu',
+    'color' => '#fff',
+    'background' => 5,
+    'type' => 1
+  ],
+  'presets' => [
+    'hypixel' => [
+      'address' => 'mc.hypixel.net',
+      'font' => 'minecraftia',
+      'color' => '#e0b450',
+      'background' => 5,
+      'type' => 1
+    ]
+  ],
+];
