19f3d13f797ce74b74296cec0511ea9ba3a40ae4
MrAnonymusz <r1pp3r444@gmail.com>
2020-03-17 16:59:14 +0200

    Updated to Version 1.3.1

---
+++ README.md
@@ -0,0 +1,95 @@
+# SMSBG (Simple Minecraft Server Banner Generator)
+
+*As the name of this project is it's just a simple php script which allows you to create custom banners for your minecarft server. These are live banners so they display live stats of your server. At the moment they display the address of your server, the amount of online players and the version of the server.
+But you may configure these options in the `banner.php` file.*
+
+### Some additional information
+
+* By default pretty urls are disabled and you can make a simple request like this: __banner.php?address=localhost:25565__. You can enable pretty urls in the `config.php` by changing the `enable_htaccess` from __false__ to __true__.
+* For prettier urls you can make a request without include the file's extension _(Instead of __banner.php?address=localhost:25565__ you can simply just __banner?address=localhost:25565__)_.
+* This project also includes a server query using [mcsrvstat.us](https://mcsrvstat.us/) api.
+* If you enable the `enable_key` option in the config file you can restrict the usage of the banner generator but then you must need to pass the following value for the future request's: `&key=your_app_key`. You can find the `app_key` in the `config.php` file. If you want to use it it's recommended to change it frequently but the actual usage of it is to disable the momentary usage of the generator.
+* This project includes 6 default banners (1-6) and 3 default fonts (Roboto, Ubuntu, Minecraftia)
+* **_Some images are used from [Tanner77](https://www.planetminecraft.com/texture_pack/vividhd-512x/)!_**
+
+### Installation
+
+**Just simply drag all the files included form the downloaded zip in to a folder and after the go to the `config.php` and change the `app_url` from `http://localhost/banner/` to the domain for the banner generator. _And if you want prettier urls to work rename the `htaccess.txt` to `.htaccess` and you are ready to go!_**
+
+### How can you make a request?!
+
+* You can get a custom banner by accessing: `banner?address=your_minecraft_server_ip&background=1&font=roboto&color=27AE60&type=1`
+* You can access the server query by: `query?address=your_minecraft_server_ip`
+* __Note 1:__ You can now simply request a banner by `banner?address=your_minecraft_server_ip` and the default settings in the `config.php` will be applied.
+* __Note 2:__ Now you can also choose from 3 predefined styles for the banner _(type)_, the default type for the banner is 1. Example: `banner?address=your_minecraft_server_ip&type=2`
+
+### How can you add a new banner background?!
+
+**The banner's resolution must be 468x60 and it must a `jpg` file and included in the `img/background` folder with the following name: `banner_(here a custom number).jpg`, example: `banner_11.jpg`. _You can set the background of the banner by changing the `background` variable for example: `banner?address=your_minecraft_server_ip&background=11&font=roboto`_.**
+
+### How can you add a new font for the banner?!
+
+**First you need to add a add your font file to the `fonts` folder and rename it like this: `examplefont-regular.ttf` after this go to the `config.php` and add it to the `fonts` and the name you specify there should be just the font's name without the _-regular.ttf_ and you must make sure it's all lowercase _(The name specified in the config and the actual name of the font file must be all lowercase)_. And now you can change the font by simply changing the `font` variable in the request url for example: `banner?address=your_minecraft_server_ip&background=1&font=your_custom_font`.**
+
+### How can you request a presetted banner?!
+
+* __You can request a custom banner specified in the `config.php` like this: `banner?preset=hypixel` *(This is the default banner preset in the `config.php` and you can use it as an example)*__
+* You can add a new preset in the `config.php` by following the example given in the file _(hypixel)_ situated in the **presets** array 
+
+#### Default config
+
+```php
+<?php
+
+$config = [
+  'app_key' => 'K6TdeFXHuqZC7Fam',
+  'enable_key' => false,
+  'query_api' => 'https://api.mcsrvstat.us/2/',
+  'app_url' => 'http://localhost/banner/',
+  'enable_htaccess' => false,
+  'fonts' => ['ubuntu', 'roboto', 'minecraftia'],
+  'defaults' => [
+    'font' => 'ubuntu',
+    'color' => '#fff',
+    'background' => 1,
+    'type' => 1
+  ],
+  'presets' => [
+    'hypixel' => [
+      'name' => 'Hypixel',
+      'address' => 'mc.hypixel.net',
+      'font' => 'minecraftia',
+      'color' => '#e0b450',
+      'background' => 4,
+      'type' => 1
+    ]
+  ],
+];
+```
+
+### Request Examples
+
+* __Banner:__ `http://localhost/banner/banner?address=mc.hypixel.net` _(Note: you may specify other parameters like `name`, `font`, `type`, `background` or `color`)_
+* __Query:__ `http://localhost/banner/query?address=mc.hypixel.net`
+
+##### Example Banner
+
+**Default Banner**
+* ![Default Banner](https://i.ibb.co/MkmYRKW/banner-example.jpg)
+
+**Default Presetted Banner**
+* ![Default Presetted Banner](https://i.ibb.co/PMRkskY/banner-preset.jpg)
+
+### Nginx Config (Optional)
+
+```nginx
+# nginx configuration
+
+autoindex off;
+
+location / {
+  if (!-e $request_filename){
+    rewrite ^/([^\.]+)$ /$1.php break;
+  }
+}
+```
---
+++ banner.php
@@ -0,0 +1,334 @@
+<?php
+ob_start();
+
+error_reporting(E_ALL & ~E_NOTICE);
+
+require_once(__DIR__.'\vendor\autoload.php');
+require_once(__DIR__.'\config.php');
+
+use Intervention\Image\ImageManagerStatic as Image;
+
+class bannerGenerator
+{
+  private $_preset;
+  private $_address;
+  private $_name;
+  private $_bakground;
+  private $_font;
+  private $_color;
+  private $_type;
+  private $_key;
+  private $_file_name;
+
+  /*
+    >> This function generates the banner image
+  */
+
+  private function generateBanner($_address, $_name, $_background, $_font, $_color, $_type, $_key)
+  {
+    global $config;
+
+    $this->_address = $_address;
+    $this->_name = $_name;
+    $this->_background = $_background;
+    $this->_font = $_font;
+    $this->_color = $_color;
+    $this->_type = $_type;
+    $this->_key = $_key;
+
+    if($config['enable_htaccess'] == true)
+    {
+      $this->_file_name = 'query';
+    }
+    else
+    {
+      $this->_file_name = 'query.php';
+    }
+
+    if($config['enable_key'] == true)
+    {
+      $this->query = json_decode(file_get_contents($config['app_url'].$this->_file_name.'?address='.$this->_address.'&type=ping&key='.$config['app_key']));
+    }
+    else
+    {
+      $this->query = json_decode(file_get_contents($config['app_url'].$this->_file_name.'?address='.$this->_address.'&type=ping'));
+    }
+
+    $this->banner_background = __DIR__.'/img/background/banner_'.$this->_background.'.jpg';
+    $this->banner_font = __DIR__.'/fonts/'.$this->_font.'-regular.ttf';
+
+    $img = Image::make($this->banner_background);
+
+    $this->final_name = $this->_name == "" ? strtoupper($this->_address) : $this->_name.' | '.strtoupper($this->_address);
+
+    if($this->query->online == "true")
+    {
+      if(!empty($this->query->icon))
+      {
+        $this->server_icon = Image::make($this->query->icon)->resize(54, 54)->encode('png');
+      }
+      else
+      {
+        $this->server_icon = Image::make(__DIR__.'/img/icon/server-icon.png')->resize(54, 54)->greyscale()->encode('png');
+      }
+
+      switch($this->_type)
+      {
+        case 1:
+          $img->text($this->final_name, 74, 12, function($font) {
+            $font->file($this->banner_font);
+            $font->size(14);
+            $font->color($this->_color);
+            $font->align('left');
+            $font->valign('top');
+          });
+
+          $img->text('Version: '.$this->query->version, 74, 32, function($font) {
+            $font->file($this->banner_font);
+            $font->size(12);
+            $font->color('#fff');
+            $font->align('left');
+            $font->valign('top');
+          });
+
+          $img->text('Players: '.$this->query->players->online.' / '.$this->query->players->max, 74, 50, function($font) {
+            $font->file($this->banner_font);
+            $font->size(12);
+            $font->color('#fff');
+            $font->align('left');
+            $font->valign('top');
+          });
+          break;
+        case 2:
+          $img->text($this->final_name, 74, 12, function($font) {
+            $font->file($this->banner_font);
+            $font->size(14);
+            $font->color($this->_color);
+            $font->align('left');
+            $font->valign('top');
+          });
+
+          $img->text('Version: '.$this->query->version, 74, 32, function($font) {
+            $font->file($this->banner_font);
+            $font->size(12);
+            $font->color('#fff');
+            $font->align('left');
+            $font->valign('top');
+          });
+
+          $img->text('Online: '.$this->query->players->online, 74, 50, function($font) {
+            $font->file($this->banner_font);
+            $font->size(12);
+            $font->color('#fff');
+            $font->align('left');
+            $font->valign('top');
+          });
+          break;
+        case 3:
+          $img->text($this->final_name, 74, 19, function($font) {
+            $font->file($this->banner_font);
+            $font->size(14);
+            $font->color($this->_color);
+            $font->align('left');
+            $font->valign('top');
+          });
+
+          $img->text('Version: '.$this->query->version, 74, 39, function($font) {
+            $font->file($this->banner_font);
+            $font->size(12);
+            $font->color('#fff');
+            $font->align('left');
+            $font->valign('top');
+          });
+          break;
+      }
+
+      $img->insert($this->server_icon, 'left', 10, 10);
+    }
+    else
+    {
+      $this->server_icon = Image::make(__DIR__.'/img/icon/offline.png')->resize(54, 54)->greyscale();
+
+      $this->offline_mark = Image::make(__DIR__.'/img/icon/times.png')->resize(27, 27);
+
+      $this->server_icon->insert($this->offline_mark, 'center');
+
+      $this->server_icon->encode('png');
+
+      $img->text($this->final_name, 74, 19, function($font) {
+        $font->file($this->banner_font);
+        $font->size(14);
+        $font->color('#fff');
+        $font->align('left');
+        $font->valign('top');
+      });
+
+      $img->text('Server is offline!', 74, 39, function($font) {
+        $font->file($this->banner_font);
+        $font->size(12);
+        $font->color('#fff');
+        $font->align('left');
+        $font->valign('top');
+      });
+
+      $img->insert($this->server_icon, 'left', 10, 10);
+    }
+
+    $render = (string) $img->encode('jpg');
+
+    return $render;
+  }
+
+  /*
+    >> This function check all the inputed variables and loads the banner
+  */
+
+  public function __construct()
+  {
+    global $config;
+
+    $this->_preset = $_GET['preset'];
+    $this->_address = $_GET['address'];
+    $this->_name = $_GET['name'];
+    $this->_background = $_GET['background'];
+    $this->_font = strtolower($_GET['font']);
+    $this->_color = $_GET['color'];
+    $this->_type = $_GET['type'];
+
+    $this->allowed_fonts = $config['fonts'];
+
+    if(empty($this->_preset))
+    {
+      if(empty($this->_address))
+      {
+        $this->error = 1;
+        $this->error_message = "Please specify a server address for the query!";
+      }
+
+      if(!empty($this->_background))
+      {
+        if(!filter_var($this->_background, FILTER_VALIDATE_INT))
+        {
+          $this->error = 1;
+          $this->error_message = "Invalid value given for the background!";
+        }
+        else
+        {
+          $this->check_dir = array_diff(scandir(__DIR__.'/img/background'), ['..', '.']);
+
+          if(!in_array('banner_'.$this->_background.'.jpg', $this->check_dir))
+          {
+            $this->error = 1;
+            $this->error_message = "Invalid value given for the background!";
+          }
+        }
+      }
+      else
+      {
+        $this->_background = $config['defaults']['background'];
+      }
+
+      if(!empty($this->_font))
+      {
+        if(!in_array($this->_font, $this->allowed_fonts))
+        {
+          $this->error = 1;
+          $this->error_message = "Invalid value given for the font!";
+        }
+      }
+      else
+      {
+        $this->_font = $config['defaults']['font'];
+      }
+
+      if(empty($this->_color))
+      {
+        $this->_color = $config['defaults']['color'];
+      }
+      else
+      {
+        $this->_color = '#'.$_GET['color'];
+      }
+
+      if(!empty($this->_type))
+      {
+        if(!filter_var($this->_type, FILTER_VALIDATE_INT))
+        {
+          $this->error = 1;
+          $this->error_message = "Invalid value given for the banner type!";
+        }
+        else if(!in_array($this->_type, [1, 2, 3]))
+        {
+          $this->error = 1;
+          $this->error_message = "Invalid value given for the banner type!";
+        }
+      }
+      else
+      {
+        $this->_type = $config['defaults']['type'];
+      }
+
+      if($config['enable_key'] == true)
+      {
+        $this->_key = $_GET['key'];
+
+        if(!empty($this->_key))
+        {
+          if($this->_key != $config['app_key'])
+          {
+            $this->error = 1;
+            $this->error_message = "The specified key is invalid!";
+          }
+        }
+        else
+        {
+          $this->error = 1;
+          $this->error_message = "Please specify the app key!";
+        }
+      }
+
+      if($this->error != 1)
+      {
+        // Render Banner
+        header('Content-Type: image/jpeg');
+
+        echo $this->generateBanner($this->_address, $this->_name, $this->_background, $this->_font, $this->_color, $this->_type, $this->_key);
+      }
+      else
+      {
+        echo json_encode([
+          'error' => $this->error_message
+        ]);
+      }
+    }
+    else
+    {
+      if(in_array($this->_preset, array_keys($config['presets'])))
+      {
+        $this->get_preset = $config['presets'][$this->_preset];
+
+        $this->_address = $this->get_preset['address'];
+        $this->_name = $this->get_preset['name'];
+        $this->_background = $this->get_preset['background'];
+        $this->_font = $this->get_preset['font'];
+        $this->_color = $this->get_preset['color'];
+        $this->_type = $this->get_preset['type'];
+
+        // Render Banner
+        header('Content-Type: image/jpeg');
+
+        echo $this->generateBanner($this->_address, $this->_name, $this->_background, $this->_font, $this->_color, $this->_type, $this->_key);
+      }
+      else
+      {
+        echo json_encode([
+          'error' => 'Sorry but we couldn\'t find any preset with this name!'
+        ]);
+      }
+    }
+  }
+}
+
+$banner = new bannerGenerator;
+
+ob_end_flush();
